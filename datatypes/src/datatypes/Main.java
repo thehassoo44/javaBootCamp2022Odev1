24a7083b911d14d035f2ce9c6a2401949e5e305a
thehassoo44 <78402141+thehassoo44@users.noreply.github.com>
2022-09-19 21:16:59 +0300

    dataTypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		sayi = 14.5;
+		
+		char karakter = 'A';
+
+		boolean dogruMu = false;
+	}
+
+}
